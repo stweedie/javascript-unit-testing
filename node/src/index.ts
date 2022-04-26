6bd91924094e6188cb965c7f0fa05d331ad120e0
Skyler Tweedie <skylertweedie@gmail.com>
2022-04-26 13:49:22 -0500

    placeholder -> todo list

--- node/src/index.ts
+++ node/src/index.ts
@@ -1,4 +1,4 @@
-import PlaceholderService from "./services/placeholder/PlaceholderService";
+import PlaceholderService from "./services/todo-list/PlaceholderService";
 
 const service = new PlaceholderService();
 service.getAllTodos()
